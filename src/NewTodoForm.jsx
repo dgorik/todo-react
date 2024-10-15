f587e76d601ffdac5efd9592fd2ccf3724944afd
Daniel Gorbachev <dg186533@gmail.com>
2024-10-15 06:08:17 -0400

    Reworked the styles

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -16,7 +16,7 @@ export function NewTodoForm(props) {
     return (
         <form onSubmit={handleSubmit} className="new-item-form">
             <div className="form-row">
-                <h2> This is your todo app</h2>
+                <h2> This is your todo-zen 🧘‍♀️</h2>
                 <label htmlFor="item"> New Item </label>
                 <input
                     value={newItem}
--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -3,7 +3,7 @@ import { TodoItem } from "./TodoItem"
 export function Todolist({ todos, toggleTodo, deleteTodo }) {
     return (
         <ul className=" list">
-            {todos.length === 0 && "Add your first todo"}
+            {todos.length === 0 && "Add your first todo..."}
             {todos.map(todo => {
                 return (
                     < TodoItem
